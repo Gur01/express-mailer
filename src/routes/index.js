ceeaac20883a55f354ea24846f3c6a2f76457164
Gur01 <v-gurov@yandex.ru>
2022-03-15 19:39:45 +0300

    add nodemailer

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,35 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'air.cleaning.profi@gmail.com',
+    pass: '13420404'
+  }
+});
+
+const mailOptions = {
+  from: 'air.cleaning.profi@gmail.com',
+  to: 'v-gurov@yandex.ru',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
 
 const router = express.Router();
 
 router.post('/api/mail', function(req, res, next) {
   console.log(req.body);
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
   res.json('index').end();
 });
 
